9379f1d55d98575dea0b6131ca6dd5f5ecd5e866
bushral <45239223+bushral@users.noreply.github.com>
2021-08-14 20:11:26 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,8 +9,14 @@ $subject = "Mail From swimming website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
+
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    $maill=mail($to,$subject,$txt,$headers);
+      if( $maill == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
     header("Location:thankyou.html");
 }
 //redirect
