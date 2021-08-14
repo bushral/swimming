89925bf2c4d1292ba0a468902fe92eb6b2e9608d
bushral <45239223+bushral@users.noreply.github.com>
2021-08-14 19:54:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+$msg = "First line of text\nSecond line of text";
 
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("alnbarybushra@gmail.com","My subject",$msg);
 include_once("HomePage.html");
 ?>
